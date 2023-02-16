b008417a5aa3b12e60426cb4a0fb436b3c479d96
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2023-02-16 21:06:30 +0300

    refactor: constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
-export const SECRET_KEY = 'verySecretKey-01.02.2023';
-
 export const reshapingOptions = {
   virtuals: true,
   transform: function (_doc: any, ret: { password: any; _id: any }) {
--- src/services/token-service.ts
+++ src/services/token-service.ts
@@ -1,15 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import jwt from 'jsonwebtoken';
 import { ITokenPayload } from '../models/types/TokenPayload';
-import { SECRET_KEY } from '../constants';
 import { JwtPayload } from '../models/types/JwtPayload';
 
 const generateAccessToken = (payload: JwtPayload): string => {
-  return jwt.sign(payload, SECRET_KEY, { expiresIn: '60m' });
+  return jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '60m' });
 };
 
 const validateAccessToken = (token: string): Promise<ITokenPayload> => {
   return new Promise((resolve, reject) => {
-    jwt.verify(token, SECRET_KEY, (error, decoded: ITokenPayload) => {
+    jwt.verify(token, process.env.SECRET_KEY, (error, decoded: ITokenPayload) => {
       if (error) return reject(error);
       resolve(decoded);
     });
